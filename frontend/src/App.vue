90377d3fb8b959e3710312d78b0fa7b072f460b5
김시은 <sieun2580@naver.com>
2024-05-19 14:53:29 +0900

    add : frontend를 위한 vue 초기 세팅

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<style>
+@font-face {
+  font-family: 'SUIT-Regular';
+  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'NanumBarunGothic';
+  font-style: normal;
+  font-weight: 400;
+  src: url('//fastly.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.eot');
+  src: url('//fastly.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.eot?#iefix') format('embedded-opentype'), url('//fastly.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.woff') format('woff'), url('//fastly.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.ttf') format('truetype');
+}
+
+body {
+  font-family: 'SUIT-Regular','NanumBarunGothic' !important;
+}
+</style>
+
+<script>
+
+</script>
