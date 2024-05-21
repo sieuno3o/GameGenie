bc138b8a4d1db40f4f97925b84cebdccc733c425
김시은 <sieun2580@naver.com>
2024-05-21 19:48:40 +0900

    feat : navbar 구현

--- frontend/src/layout/TheNavbar.vue
+++ frontend/src/layout/TheNavbar.vue
@@ -1,13 +1,54 @@
 <template>
-  <div id="navber" class="flex-center">
-    navbar
+  <div id="navbar" class="flex-center">
+    <router-link to="/">
+      <img src="../assets/image/logo2.png" class="navLogoImg">
+    </router-link>
+    <router-link to="/community">
+      <span class="navCommunity">커뮤니티</span>
+    </router-link>
+    <div class="navAccount">
+      <router-link to="/login">
+        <span class="navLogin">로그인</span>
+      </router-link>
+      <router-link to="/signup">
+        <span class="navSignup">회원가입</span>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TheNavber",
+  name: "TheNavbar",
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#navbar {
+  width: auto;
+  height: 55px;
+  background-color: $MAIN-COLOR-SKYBLUE;
+  justify-content: space-between;
+}
+
+.navLogoImg {
+  width: 130px;
+  margin-top: -4px;
+  padding-left: 25px;
+}
+
+a, 
+a.router-link-exact-active,
+a.router-link-active {
+  text-decoration: none;
+  color: inherit;
+}
+
+.navAccount {
+  padding-right: 25px;
+}
+
+.navLogin {
+  padding-right: 30px;
+}
+</style>
