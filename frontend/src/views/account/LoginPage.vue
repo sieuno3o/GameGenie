aa9b97335eb34fe96551f7000bfc9935b24cd6c5
김시은 <sieun2580@naver.com>
2024-05-21 17:16:27 +0900

    add : 파일 생성

---
+++ frontend/src/views/account/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    로그인페이지~~!
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/account/SignupPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    회원가입 페이지!!
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped></style>
