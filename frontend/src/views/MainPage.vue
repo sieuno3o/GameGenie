090f873bc7f9af1211edde3622c48d212d036e8e
김시은 <sieun2580@naver.com>
2024-05-22 03:18:34 +0900

    feat : main page 구현

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    메인페이지!!!!!!!
+  <div class="backgroundMain flex-col-center">
+    <img src="../assets/image/logo2.png" class="mainLogoImg">
+    <div class="mainSearchbar flex-left">
+      <img src="../assets/image/searchIcon.png" class="searchIcon">
+      <input type="text" class="mainSearchInput body1" placeholder="장르 또는 게임 이름 검색">
+    </div>
   </div>
 </template>
 
@@ -8,5 +12,35 @@
 </script>
 
 <style lang="scss" scoped>
+.backgroundMain {
+  width: 100vw;
+  height: 100vh;
+  background-color: $MAIN-COLOR-SKYBLUE;
+}
 
+.mainLogoImg {
+  margin-top: -200px;
+  width: 480px;
+}
+
+.mainSearchbar {
+  margin-top: 30px;
+  background-color: white;
+  width: 800px; height: 80px;
+  border-radius: 90px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding-left: 30px;
+}
+
+.mainSearchInput {
+  border: 0;
+  outline: none;
+  width: 80%;
+  height: 100%;
+  padding-left: 20px;
+}
+
+.searchIcon {
+  width: 20px;
+}
 </style>
