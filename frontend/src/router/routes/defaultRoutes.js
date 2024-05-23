717d87a7f6da9f075f0c60b2388a415df4708be0
김시은 <sieun2580@naver.com>
2024-05-24 02:41:21 +0900

    fix : 추천 상세 페이지 route 설정

--- frontend/src/router/routes/defaultRoutes.js
+++ frontend/src/router/routes/defaultRoutes.js
@@ -1,11 +1,13 @@
-import defaultDashboard from "@/layout/dashboard/DefaultDashboard";
+import MainPageDashboard from "@/layout/dashboard/MainDashboard.vue";
+import DefaultDashboard from "@/layout/dashboard/DefaultDashboard.vue";
 
 const MainSearchPage = () => import("@/views/MainSearchPage.vue");
+const RecommendPage = () => import("@/views/RecommendPage.vue");
 
 const defaultRoutes = [
   {
     path: "",
-    component: defaultDashboard,
+    component: MainPageDashboard,
     children: [
       {
         path: "",
@@ -13,7 +15,18 @@ const defaultRoutes = [
         component: MainSearchPage,
       },
     ]
-    }
+  },
+  {
+    path: "recommend",
+    component: DefaultDashboard,
+    children: [
+      {
+        path: "",
+        name: "recommend",
+        component: RecommendPage,
+      },
+    ]
+  }
 ]
 
 export default defaultRoutes;
