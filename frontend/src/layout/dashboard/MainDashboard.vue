76c1ed904e96d22cf694a8ede03252cda3eccc0d
김시은 <sieun2580@naver.com>
2024-05-24 02:40:49 +0900

    add : main dashboard 생성

---
+++ frontend/src/layout/dashboard/MainDashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex-center" id="contents-wrapper">
+    <div class="flex-row-around" id="contents">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultDashboard",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+#contents {
+  max-width: 1920px;
+  min-height: calc(100vh - 130px - 70px);
+  margin-top: 120px
+}
+
+#contents-wrapper {
+  background-color: $MAIN-COLOR-SKYBLUE;
+}
+</style>
