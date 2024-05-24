af2b59acbd3e6bf4f48055e84330157f0160ac9f
LaisKor <spiderjiwon@nate.com>
2024-05-24 12:19:23 +0900

    feat : RecommendPage.vue에 추천 기능 연동

--- backend/GameGenie/settings.py
+++ backend/GameGenie/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders',
     'django_filters',
     "accounts",
     "community",
