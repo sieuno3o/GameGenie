8a6467f662fad91ad94c29096c71d7141e96ab61
김시은 <sieun2580@naver.com>
2024-05-31 04:04:32 +0900

    fix : 카드 5개에서 4개 띄우도록 수정

--- frontend/src/views/RecommendPage.vue
+++ frontend/src/views/RecommendPage.vue
@@ -2,18 +2,8 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <v-text-field
-          v-model="userInput"
-          label="검색어를 입력하세요"
-          @keyup.enter="sendQuery"
-        ></v-text-field>
-        <v-btn @click="sendQuery">검색</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
         <div v-for="(message, index) in messages" :key="index" class="chat-message">
-          <div :class="{'user-message': message.isUser, 'bot-message': !message.isUser}">
+          <div :class="{ 'user-message': message.isUser, 'bot-message': !message.isUser }">
             {{ message.text }}
           </div>
         </div>
@@ -37,6 +27,16 @@
         </v-row>
       </v-col>
     </v-row>
+    <!-- 검색어 입력 부분을 화면 하단에 고정 -->
+    <div class="search-bar">
+      <v-row>
+        <v-col cols="12">
+          <v-text-field v-model="userInput" label="검색어를 입력하세요" @keyup.enter="sendQuery" append-outer-icon="mdi-magnify"
+            @click:append-outer="sendQuery">
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -72,7 +72,7 @@ export default {
 
         if (data.similar_games) {
           this.messages.push({ text: '다음은 추천 게임입니다:', isUser: false });
-          this.recommendedGames = data.similar_games;
+          this.recommendedGames = data.similar_games.slice(0, 4);
         } else {
           this.messages.push({ text: data.error || '추천 게임을 찾을 수 없습니다.', isUser: false });
         }
@@ -99,7 +99,7 @@ export default {
 
         if (data.similar_games) {
           this.messages.push({ text: '다음은 추가 추천 게임입니다:', isUser: false });
-          this.recommendedGames = data.similar_games;
+          this.recommendedGames = data.similar_games.slice(0, 4);
         } else {
           this.messages.push({ text: data.error || '추가 추천 게임을 찾을 수 없습니다.', isUser: false });
         }
@@ -115,29 +115,50 @@ export default {
 <style lang="scss" scoped>
 .user-message {
   position: relative;
-  margin: 50px;
   padding: 20px;
-  width: 200px;
-  height: 60px;
+  min-width: 200px;
+  min-height: 60px;
   color: #FFF;
   border-radius: 10px;
   background-color: white;
-  border: 1px solid rgb(203, 203, 203);
+  border: 1px solid rgb(147, 147, 147);
   color: black;
 }
 
+.user-message:before,
 .user-message:after {
   content: "";
   position: absolute;
-  top: 21px;
-  right: -30px;
-  border-left: 30px solid rgb(255, 255, 255);
   border-top: 10px solid transparent;
   border-bottom: 10px solid transparent;
+}
+
+.user-message:before {
+  border-left: 31px solid rgb(147, 147, 147);
+  top: 21px;
+  right: -33px;
+  z-index: 1;
+}
+
+.user-message:after {
+  border-left: 30px solid white;
+  top: 21px;
+  right: -30px;
+  z-index: 2;
 }
 
 .bot-message {
   text-align: left;
   color: green;
+}
+
+.search-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: #fff;
+  padding: 10px;
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
 }
 </style>
