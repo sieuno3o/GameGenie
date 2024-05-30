c1e74e1e6e000b2f0bf16b84eccb62a003c3eb72
김시은 <sieun2580@naver.com>
2024-05-31 05:31:00 +0900

    add : recommend dashboard 추가

---
+++ frontend/src/layout/dashboard/RecommendDashBoard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex-row-around" id="contents">
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+#contents {
+  background-color: $MAIN-COLOR-SKYBLUE;
+  max-width: 1920px;
+  min-height: calc(100vh - 55px);
+}
+</style>
