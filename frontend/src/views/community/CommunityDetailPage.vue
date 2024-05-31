64156786ea52f8b4a08e869109ec382b0ba01bcd
SJ <yws7236@gmail.com>
2024-05-31 18:44:14 +0900

    feat: 커뮤니티 디테일 페이지 구현
    작성자가 author 로 뜨지만 username으로 수정 예정

--- frontend/src/views/community/CommunityDetailPage.vue
+++ frontend/src/views/community/CommunityDetailPage.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>
-    커뮤니티 디테일 페이지
+  <div class="communityProfile" v-if="communityItem">
+    <h1>게시글 상세 페이지</h1>
+    <h1>{{ communityItem.title }}</h1>
+    <p>작성자: {{ communityItem.author }}</p>
+    <p>{{ communityItem.content }}</p>
   </div>
 </template>
 
 <script>
+import api from '../../api';
+import accountsAPI from '../../accountsAPI';
+
+export default {
+  data() {
+    return {
+      communityItem: null,
+      authorName: '알 수 없음',
+    };
+  },
+  async created() {
+    const id = this.$route.params.id;
+    await this.fetchCommunityItem(id);
+  },
+  methods: {
+    async fetchCommunityItem(id) {
+      try {
+        const response = await api.get(`community/${id}/`);
+        this.communityItem = response.data;
+        const user = await accountsAPI.getUser();
+        this.author = user.username;
+      } catch (error) {
+        console.error("게시글을 가져오는 중 오류가 발생했습니다 :", error);
+      }
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.communityProfile {
+  width: 100%;
+  height: 100px;
+  font-size: 30px;
+  margin: 40px;
+  text-align: center;
+}
+</style>
