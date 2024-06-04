1f79936932a9b9a70b960eb2ab1754405652a80f
김시은 <sieun2580@naver.com>
2024-06-04 18:22:35 +0900

    feat : 드랍다운 구현

--- frontend/src/layout/TheNavbar.vue
+++ frontend/src/layout/TheNavbar.vue
@@ -11,9 +11,13 @@
         <router-link v-if="!isLoggedIn" to="/login">
           <span class="navLogin">로그인</span>
         </router-link>
-        <router-link v-else :to="`/profile/${userId}`">
-          <span class="navLogin">{{ username }}</span>
-        </router-link>
+        <div v-else class="dropdown">
+          <span class="navLogin" @click="toggleDropdown">{{ username }}</span>
+          <div v-if="isDropdownOpen" class="dropdown-content">
+            <router-link :to="`/profile/${userId}`">내 프로필</router-link>
+            <a @click="logout">로그아웃</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -27,6 +31,7 @@ export default {
       isLoggedIn: false,
       userId: null,
       username: '',
+      isDropdownOpen: false
     };
   },
   created() {
@@ -49,6 +54,17 @@ export default {
         localStorage.removeItem('userId');
         localStorage.removeItem('username');
       }
+    },
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    logout() {
+      localStorage.removeItem('access');
+      localStorage.removeItem('refresh');
+      localStorage.removeItem('userId');
+      localStorage.removeItem('username');
+      this.isLoggedIn = false;
+      this.$router.push('/login');
     }
   }
 };
@@ -60,6 +76,13 @@ export default {
   height: 55px;
   background-color: $MAIN-COLOR-SKYBLUE;
   justify-content: space-between;
+  display: flex;
+  align-items: center;
+}
+
+.flex-center {
+  display: flex;
+  align-items: center;
 }
 
 .navLogoImg {
@@ -78,10 +101,42 @@ a.router-link-active {
 .navLogin {
   padding-right: 30px;
   margin-left: 30px;
+  cursor: pointer;
 }
 
 .navCommunity:hover,
 .navLogin:hover {
   color: $HOVER-COLOR;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  position: absolute;
+  background-color: #f9f9f9;
+  max-width: 120px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  margin-top: 10px;
+  margin-left: 13px;
+  text-align: center;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+.dropdown .dropdown-content {
+  display: block;
 }
 </style>
