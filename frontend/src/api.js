c0a8387d66f4da67d146e1019261f7897ac3f882
LaisKor <spiderjiwon@nate.com>
2024-06-09 20:04:58 +0900

    fix : baseURL 수정

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://13.125.200.223/api/',  // EC2 인스턴스의 퍼블릭 IP 주소
+  baseURL: 'https://13.125.200.223/api/',  // EC2 인스턴스의 퍼블릭 IP 주소
 });
 
 api.interceptors.request.use(
