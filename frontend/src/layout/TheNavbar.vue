c6c0c356bbc4619e970900ee395a221de5f7b75c
LaisKor <spiderjiwon@nate.com>
2024-06-11 14:42:58 +0900

    fix : Navbar 에러 수정

--- frontend/src/layout/TheNavbar.vue
+++ frontend/src/layout/TheNavbar.vue
@@ -5,19 +5,19 @@
     </router-link>
     <div class="flex-center navLinks">
       <router-link to="/community">
-        <span class="navCommunity">커뮤니티</span>
+        <span class="navCommunity NAV">커뮤니티</span>
       </router-link>
       <div class="navAccount">
         <router-link v-if="!isLoggedIn" to="/login">
-          <span class="navLogin">로그인</span>
+          <span class="navLogin NAV">로그인</span>
         </router-link>
         <div v-else class="dropdown">
-          <span class="navLogin" @click="toggleDropdown">{{ nickname }}</span>
+          <span class="navLogin NAV" @click="toggleDropdown">{{ nickname }}</span>
           <div v-if="isDropdownOpen" class="dropdown-content">
-            <router-link :to="{ name: 'profile', params: { nickname: nickname } }" class="dropdown-item">
+            <router-link :to="{ name: 'profile', params: { nickname: nickname } }" class="dropdown-item flex-center">
               <span class="dropdown-font button2">내 프로필</span>
             </router-link>
-            <a @click="logout" class="dropdown-item">
+            <a @click="logout" class="dropdown-item flex-center">
               <span class="dropdown-font button2">로그아웃</span>
             </a>
           </div>
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import api from '../api';
+import api from '../api'; // Adjust the path to where your api.js is located
 
 export default {
   name: "TheNavbar",
@@ -39,8 +39,8 @@ export default {
       isDropdownOpen: false
     };
   },
-  created() {
-    this.checkLoginStatus();
+  async created() {
+    await this.checkLoginStatus();
   },
   mounted() {
     document.addEventListener('click', this.handleOutsideClick);
@@ -54,14 +54,13 @@ export default {
         const token = localStorage.getItem('access');
         if (token) {
           const response = await api.get('accounts/profile/');
-          if (response.data) {
-            this.isLoggedIn = true;
-            this.nickname = response.data.nickname;
-          } else {
-            this.isLoggedIn = false;
-          }
+          this.isLoggedIn = true;
+          this.nickname = response.data.nickname;
+        } else {
+          this.isLoggedIn = false;
         }
       } catch (error) {
+        console.error("Error in checkLoginStatus:", error);
         this.isLoggedIn = false;
       }
     },
@@ -126,9 +125,11 @@ a.router-link-active {
 }
 
 .navLogin {
-  padding-right: 30px;
-  margin-left: 15px;
   cursor: pointer;
+}
+
+.NAV {
+  margin-right: 35px;
 }
 
 .navCommunity:hover,
@@ -144,17 +145,17 @@ a.router-link-active {
 .dropdown-content {
   position: absolute;
   background-color: #f9f9f9;
-  width: 150px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  width: 110px;
+  box-shadow: 0px 5px 15px 0px rgba(0, 0, 0, 0.1);
   z-index: 1;
   margin-top: 10px;
-  margin-left: -25px;
+  margin-left: -35px;
 }
 
 .dropdown-content .dropdown-item {
   display: flex;
   align-items: center;
-  padding: 8px 12px;
+  padding: 14px 12px;
   color: black;
   text-decoration: none;
   cursor: pointer;
