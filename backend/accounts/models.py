df3dc6b788a321556b6229b21ecabd98145b7b65
LaisKor <spiderjiwon@nate.com>
2024-06-11 21:24:06 +0900

    fix : 프로필 사진 표시

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     karma = models.IntegerField(default=1)
-    nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)  # nickname 필드 수정
+    nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)
     profile_image = models.ImageField(upload_to='profile_image/', null=True, blank=True)
+
     def __str__(self):
         return self.username
