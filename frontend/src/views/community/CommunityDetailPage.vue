94101a26a18daf6c8818d7b9c74a648f0468a823
LaisKor <spiderjiwon@nate.com>
2024-06-11 19:30:24 +0900

    fix : 댓글 수정, 삭제 기능 추가

--- frontend/src/views/community/CommunityDetailPage.vue
+++ frontend/src/views/community/CommunityDetailPage.vue
@@ -10,12 +10,12 @@
     <p>{{ communityItem.content }}</p>
     <div class="likeRow">
       <button class="likeButton" @click="toggleLike">♥</button>
-      <h3>{{ likesCount }}</h3>
+      <h3>{{ communityItem.community_like ? communityItem.community_like.length : 0 }}</h3>
     </div>
     <div class="detailButtonList">
       <button class="detailButton" @click="goToCommunity">목록으로</button>
-      <v-btn v-if="isAuthor" class="detailvButton" color="primary" @click="showEditModal = true">수정하기</v-btn>
-      <button v-if="isAuthor" class="detailButton" style="background-color: #FF9393;" @click="deletePost">삭제하기</button>
+      <v-btn class="detailvButton" color="primary" @click="showEditModal = true">수정하기</v-btn>
+      <button class="detailButton" style="background-color: #FF9393;" @click="deletePost">삭제하기</button>
     </div>
     <v-dialog v-model="showEditModal" persistent max-width="600px">
       <v-card>
@@ -49,6 +49,8 @@
           <h2><strong>{{ comment.author_nickname }}</strong></h2>
           <h3>{{ formatDate(comment.created_at) }}</h3>
           <h2>{{ comment.comments }}</h2>
+          <v-btn v-if="comment.author_id === userId" small @click="editComment(comment)">수정</v-btn>
+          <v-btn v-if="comment.author_id === userId" small color="error" @click="deleteComment(comment.id)">삭제</v-btn>
         </div>
       </div>
       <div v-if="isLoggedIn">
@@ -59,12 +61,33 @@
         <p>로그인 후 댓글을 작성할 수 있습니다.</p>
       </div>
     </div>
+    <v-dialog v-model="showEditCommentModal" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">댓글 수정</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-textarea v-model="editCommentData.comments" label="내용" />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="showEditCommentModal = false">취소</v-btn>
+          <v-btn color="blue darken-1" text @click="updateComment">저장</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import api from '../../api';
-import defaultProfileImage from '@/assets/image/account/profileImgIcon.png'; // Corrected import path
+import defaultProfileImage from '@/assets/image/account/profileImgIcon.png';
 
 export default {
   data() {
@@ -81,10 +104,13 @@ export default {
         title: '',
         content: '',
       },
+      editCommentData: {
+        id: null,
+        comments: '',
+      },
+      showEditCommentModal: false,
       errorMessage: '',
-      defaultProfileImage: defaultProfileImage, // Default profile image
-      isAuthor: false, // New state to check if the user is the author
-      likesCount: 0, // New state to store likes count
+      defaultProfileImage: defaultProfileImage
     };
   },
   async created() {
@@ -98,12 +124,6 @@ export default {
       try {
         const response = await api.get(`community/${id}/`);
         this.communityItem = response.data;
-        if (!this.communityItem.community_like) {
-          this.communityItem.community_like = [];
-        }
-        this.likesCount = this.communityItem.community_like.length;
-        console.log('Fetched community item:', this.communityItem); // 로그 추가
-        this.checkAuthor();
       } catch (error) {
         console.error("게시글을 가져오는 중 오류가 발생했습니다:", error);
       }
@@ -119,7 +139,7 @@ export default {
       return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     },
     getImageUrl(imagePath) {
-      return `http://52.79.116.122${imagePath}`;
+      return `http://localhost:8000${imagePath}`;
     },
     async checkLoginStatus() {
       const token = localStorage.getItem('access');
@@ -131,21 +151,11 @@ export default {
             }
           });
           this.isLoggedIn = true;
-          this.userId = response.data.id; // userId를 user의 ID로 변경
-          console.log('Logged in user ID:', this.userId); // 로그 추가
+          this.userId = response.data.id;
         } catch (error) {
           console.error("로그인 상태를 확인하는 중 오류가 발생했습니다:", error);
         }
       }
-    },
-    checkAuthor() {
-      console.log('Checking author with community item:', this.communityItem); // 로그 추가
-      if (this.communityItem && this.communityItem.author_id === this.userId) { // author_nickname 대신 author_id 사용
-        this.isAuthor = true;
-      } else {
-        this.isAuthor = false;
-      }
-      console.log('Is Author:', this.isAuthor); // 로그 추가
     },
     async toggleLike() {
       if (!this.isLoggedIn) {
@@ -162,7 +172,6 @@ export default {
         if (response.data && typeof response.data.likes_count !== 'undefined') {
           this.likesCount = response.data.likes_count;
           this.isLiked = response.data.liked;
-          console.log('Updated likes:', this.likesCount);
         } else {
           console.error("Invalid response data", response.data);
         }
@@ -172,7 +181,7 @@ export default {
     },
     async fetchComments(id) {
       try {
-        const response = await api.get(`community/comments/${id}`);
+        const response = await api.get(`community/${id}/comments/`);
         this.comments = response.data.results;
       } catch (error) {
         console.error("댓글을 가져오는 중 오류가 발생했습니다:", error);
@@ -183,7 +192,7 @@ export default {
       if (this.newComment.trim() === '') return;
       const id = this.$route.params.id;
       try {
-        const response = await api.post(`community/comments/${id}/create/`, {
+        const response = await api.post(`community/${id}/comments/create/`, {
           comments: this.newComment
         }, {
           headers: {
@@ -195,6 +204,49 @@ export default {
       } catch (error) {
         console.error("댓글 작성 중 오류가 발생했습니다:", error);
         this.errorMessage = "댓글 작성 중 오류가 발생했습니다.";
+      }
+    },
+    editComment(comment) {
+      this.editCommentData.id = comment.id;
+      this.editCommentData.comments = comment.comments;
+      this.showEditCommentModal = true;
+    },
+    async updateComment() {
+      const { id, comments } = this.editCommentData;
+      try {
+        console.log(`Updating comment ${id} with content: ${comments}`);
+        const response = await api.patch(`community/${this.communityItem.id}/comments/${id}/`, { comments }, {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('access')}`
+          }
+        });
+        if (response.status === 200) {
+          const updatedComment = this.comments.find(c => c.id === id);
+          if (updatedComment) {
+            updatedComment.comments = comments;
+          }
+          this.showEditCommentModal = false;
+          alert('댓글이 성공적으로 수정되었습니다.');
+        }
+      } catch (error) {
+        console.error("댓글 업데이트 중 오류가 발생했습니다:", error);
+        alert('댓글 업데이트 중 오류가 발생했습니다.');
+      }
+    },
+    async deleteComment(commentId) {
+      if (confirm('정말 삭제하시겠습니까?')) {
+        try {
+          await api.delete(`community/${this.communityItem.id}/comments/${commentId}/`, {
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('access')}`
+            }
+          });
+          this.comments = this.comments.filter(comment => comment.id !== commentId);
+          alert('댓글이 성공적으로 삭제되었습니다.');
+        } catch (error) {
+          console.error("댓글 삭제 중 오류가 발생했습니다:", error);
+          alert('댓글 삭제 중 오류가 발생했습니다.');
+        }
       }
     },
     goBack() {
