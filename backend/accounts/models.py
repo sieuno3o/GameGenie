6e12670eabec654a2a2a50877beb2f7cbb5da2d6
이은규 <djkyteofficial@gmail.com>
2024-06-11 18:49:27 +0900

    fix:accounts 모델,시리얼라이저 수정

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,6 +5,6 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     karma = models.IntegerField(default=1)
     nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)  # nickname 필드 수정
-
+    profile_image = models.ImageField(upload_to='profile_image/', null=True, blank=True)
     def __str__(self):
         return self.username
--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'created_at', 'nickname']
+        fields = ['id', 'username', 'email', 'password', 'created_at', 'nickname', 'profile_image']
         read_only_fields = ['created_at']
 
     def get_author(self, obj):
@@ -17,7 +17,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=False, validators=[validate_password])
     email = serializers.EmailField(required=False)
-    profile_image = serializers.ImageField(source='profileImage', allow_null=True)
+    profile_image = serializers.ImageField(allow_null=True)
     
     class Meta:
         model = User
