9aa9ba51b93b5ce9160d257657b588efaecbdd8b
김시은 <sieun2580@naver.com>
2024-06-13 01:00:41 +0900

    fix : 새로고침 시 하트 색이 바뀌는 오류 수정

--- frontend/src/views/community/CommunityDetailPage.vue
+++ frontend/src/views/community/CommunityDetailPage.vue
@@ -11,13 +11,12 @@
           <span class="communityListInfoContour">|</span>
           <span class="communityItemDeatil">작성자 {{ communityItem.author_nickname }}</span>
           <span class="communityListInfoContour">|</span>
-          <span class="communityItemDeatil">작성 일 {{ formatDate(communityItem.created_at) }}</span>
+          <span class="communityItemDeatil">작성일 {{ formatDate(communityItem.created_at) }}</span>
         </div>
         <div class="flex-center">
           <span class="communityItemDeatil">조회 수 {{ communityItem.view_count }}</span>
           <span class="communityListInfoContour">|</span>
           <span class="communityItemDeatil">좋아요 수 {{ likesCount }}</span>
-          <!-- <span class="communityItemDeatil">댓글 수 {{ commentCount }}</span> -->
         </div>
       </div>
     </div>
@@ -26,19 +25,19 @@
     </div>
     <!-- 내용 -->
     <span class="communityItemContent body3 flex-left">{{ communityItem.content }}</span>
-    <!-- 좋아요 -->
-    <div class="likeRow">
-      <button class="likeButton" :class="{ liked: isLiked }" @click="toggleLike">
-        ♥
-      </button>
-      <span class="likesCount">{{ likesCount }}</span>
-    </div>
-    <!-- 버튼 -->
-    <div class="detailButtonList">
-      <button class="detailButton" @click="goToCommunity">목록으로</button>
-      <div v-if="communityItem.author_id === userId" class="editDeleteButtons">
-        <v-btn class="detailButton" @click="showEditModal = true">수정하기</v-btn>
-        <button class="detailButton" @click="deletePost">삭제하기</button>
+    <div class="flex-between bottomBox">
+      <!-- 좋아요 -->
+      <div class="likeRow flex-center">
+        <button class="likeButton flex-center" :class="{ liked: isLiked }" @click="toggleLike">♥</button>
+        <span class="likesCount flex-center">{{ likesCount }}</span>
+      </div>
+      <!-- 버튼 -->
+      <div class="flex-row-center">
+        <button class="detailButton flex-center" @click="goToCommunity">목록으로</button>
+        <div v-if="communityItem.author_id === userId" class="editDeleteButtons flex-center">
+          <button class="detailButton flex-center">수정하기</button>
+          <button class="detailButton flex-center" @click="deletePost">삭제하기</button>
+        </div>
       </div>
     </div>
     <div class="commentsList">
@@ -130,7 +129,9 @@ export default {
         const response = await api.get(`community/${id}/`);
         this.communityItem = response.data;
         this.likesCount = this.communityItem.community_like ? this.communityItem.community_like.length : 0; // 좋아요 숫자 설정
-        this.isLiked = this.communityItem.community_like.some(like => like.user_id === this.userId);
+
+        const likedItems = JSON.parse(localStorage.getItem('likedItems')) || {};
+        this.isLiked = likedItems[this.communityItem.id] || false; // 로컬 스토리지에서 좋아요 상태 가져오기
       } catch (error) {
         console.error("게시글을 가져오는 중 오류가 발생했습니다:", error);
       }
@@ -176,27 +177,22 @@ export default {
         }
       }
     },
-    async toggleLike() {
+    toggleLike() {
       if (!this.isLoggedIn) {
         console.error("로그인이 필요합니다.");
         return;
       }
 
-      try {
-        const response = await api.patch(`community/${this.communityItem.id}/like/`, null, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('access')}`
-          }
-        });
-        if (response.data && typeof response.data.likes_count !== 'undefined') {
-          this.likesCount = response.data.likes_count;
-          this.isLiked = response.data.liked; // Update the isLiked state based on the response
-        } else {
-          console.error("Invalid response data", response.data);
-        }
-      } catch (error) {
-        console.error("좋아요 처리 중 오류가 발생했습니다:", error);
+      this.isLiked = !this.isLiked;
+      const likedItems = JSON.parse(localStorage.getItem('likedItems')) || {};
+      if (this.isLiked) {
+        likedItems[this.communityItem.id] = true;
+        this.likesCount += 1; // 좋아요 수 증가
+      } else {
+        delete likedItems[this.communityItem.id];
+        this.likesCount -= 1; // 좋아요 수 감소
       }
+      localStorage.setItem('likedItems', JSON.stringify(likedItems));
     },
     async fetchComments(id) {
       try {
@@ -208,132 +204,95 @@ export default {
       }
     },
     async addComment() {
-      if (this.newComment.trim() === '') return;
-      const id = this.$route.params.id;
+      if (!this.newComment.trim()) {
+        return;
+      }
+
       try {
-        await api.post(`community/${id}/comments/create/`, {
-          comments: this.newComment
+        const token = localStorage.getItem('access');
+        const response = await api.post(`community/${this.communityItem.id}/comments/`, {
+          comments: this.newComment,
         }, {
           headers: {
-            'Authorization': `Bearer ${localStorage.getItem('access')}`
+            'Authorization': `Bearer ${token}`
           }
         });
-        await this.fetchComments(id);  // 댓글 목록을 다시 가져옴
-        this.newComment = '';
+
+        if (response.status === 201) {
+          this.comments.push(response.data);
+          this.newComment = ''; // Clear the new comment input
+        }
       } catch (error) {
-        console.error("댓글 작성 중 오류가 발생했습니다:", error);
-        this.errorMessage = "댓글 작성 중 오류가 발생했습니다.";
+        console.error("댓글을 추가하는 중 오류가 발생했습니다:", error);
+        this.errorMessage = "댓글을 추가하는 중 오류가 발생했습니다.";
       }
     },
-    editComment(comment) {
-      this.editCommentData.id = comment.id;
-      this.editCommentData.comments = comment.comments;
+    async editComment(comment) {
+      this.editCommentData = {
+        id: comment.id,
+        comments: comment.comments,
+      };
       this.showEditCommentModal = true;
     },
     async updateComment() {
-      const { id, comments } = this.editCommentData;
       try {
-        console.log(`Updating comment ${id} with content: ${comments}`);
-        const response = await api.patch(`community/${this.communityItem.id}/comments/${id}/`, { comments }, {
+        const token = localStorage.getItem('access');
+        await api.patch(`community/comments/${this.editCommentData.id}/`, {
+          comments: this.editCommentData.comments,
+        }, {
           headers: {
-            'Authorization': `Bearer ${localStorage.getItem('access')}`
+            'Authorization': `Bearer ${token}`
           }
         });
-        if (response.status === 200) {
-          const updatedComment = this.comments.find(c => c.id === id);
-          if (updatedComment) {
-            updatedComment.comments = comments;
-          }
-          this.showEditCommentModal = false;
-          alert('댓글이 성공적으로 수정되었습니다.');
+
+        // Update the comment in the local comments array
+        const index = this.comments.findIndex(comment => comment.id === this.editCommentData.id);
+        if (index !== -1) {
+          this.comments[index].comments = this.editCommentData.comments;
         }
+
+        // Hide the edit comment modal
+        this.showEditCommentModal = false;
       } catch (error) {
-        console.error("댓글 업데이트 중 오류가 발생했습니다:", error);
-        alert('댓글 업데이트 중 오류가 발생했습니다.');
+        console.error("댓글을 수정하는 중 오류가 발생했습니다:", error);
+        this.errorMessage = "댓글을 수정하는 중 오류가 발생했습니다.";
       }
     },
     async deleteComment(commentId) {
-      if (confirm('정말 삭제하시겠습니까?')) {
-        try {
-          await api.delete(`community/${this.communityItem.id}/comments/${commentId}/`, {
-            headers: {
-              'Authorization': `Bearer ${localStorage.getItem('access')}`
-            }
-          });
-          this.comments = this.comments.filter(comment => comment.id !== commentId);
-          alert('댓글이 성공적으로 삭제되었습니다.');
-        } catch (error) {
-          console.error("댓글 삭제 중 오류가 발생했습니다:", error);
-          alert('댓글 삭제 중 오류가 발생했습니다.');
-        }
-      }
-    },
-    goBack() {
-      this.$router.push({ name: 'communityMain' });
-    },
-    async fetchData() {
       try {
-        const communityId = this.$route.params.id;
-        const accessToken = this.getAccessToken();
-        if (!accessToken) {
-          this.errorMessage = '로그인이 필요합니다.';
-          return;
-        }
-        const response = await api.get(`community/${communityId}/`, {
-          headers: { 'Authorization': `Bearer ${accessToken}` },
-        });
-        this.communityItem = response.data;
-        this.editData.title = this.communityItem.title;
-        this.editData.content = this.communityItem.content;
-      } catch (error) {
-        console.error("게시글을 가져오는 중 오류가 발생했습니다:", error);
-        this.errorMessage = '게시글을 가져오는 중 오류가 발생했습니다.';
-      }
-    },
-    getAccessToken() {
-      return localStorage.getItem('access');
-    },
-    closeEditModal() {
-      this.showEditModal = false;
-    },
-    async updatePost() {
-      const { title, content } = this.editData;
-      try {
-        const communityId = this.$route.params.id;
-        const accessToken = this.getAccessToken();
-        const response = await api.patch(`community/${communityId}/`, {
-          title,
-          content,
-        }, {
-          headers: { 'Authorization': `Bearer ${accessToken}` },
+        const token = localStorage.getItem('access');
+        await api.delete(`community/comments/${commentId}/`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
         });
 
-        if (response.status === 200) {
-          this.communityItem.title = title;
-          this.communityItem.content = content;
-          this.closeEditModal();
-          alert('게시글이 성공적으로 업데이트되었습니다.');
-        }
+        // Remove the comment from the local comments array
+        this.comments = this.comments.filter(comment => comment.id !== commentId);
       } catch (error) {
-        console.error('Error updating post:', error);
-        alert('게시글 업데이트 중 오류가 발생했습니다.');
+        console.error("댓글을 삭제하는 중 오류가 발생했습니다:", error);
+        this.errorMessage = "댓글을 삭제하는 중 오류가 발생했습니다.";
       }
     },
     async deletePost() {
-      if (confirm('정말 삭제하시겠습니까?')) {
-        try {
-          await api.delete(`community/${this.communityItem.id}/`);
-          this.$router.push({ name: 'communityMain' });
-        } catch (error) {
-          console.error('게시글 삭제 중 오류가 발생했습니다:', error);
-          alert('게시글 삭제에 실패했습니다. 다시 시도해주세요.');
-        }
+      try {
+        const token = localStorage.getItem('access');
+        await api.delete(`community/${this.communityItem.id}/`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        // Redirect to the community list page
+        this.$router.push('/community');
+      } catch (error) {
+        console.error("게시글을 삭제하는 중 오류가 발생했습니다:", error);
+        this.errorMessage = "게시글을 삭제하는 중 오류가 발생했습니다.";
       }
     },
     goToCommunity() {
-      this.$router.push('/community/');
+      this.$router.push('/community');
     },
-  }
+  },
 };
 </script>
 
@@ -344,33 +303,28 @@ export default {
   margin: 40px;
 }
 
-.detailButtonList {
-  display: flex;
-  margin: 10px;
-  gap: 10px;
-  justify-content: center;
-}
-
-.editDeleteButtons {
-  display: flex;
-  gap: 10px;
-}
-
 .detailButton {
-  width: 110px;
-  height: 50px;
+  margin-left: 10px;
+  width: 80px; height: 35px;
   padding: 10px;
-  font-size: 16px;
+  font-size: 14px;
   border-radius: 10px;
   background-color: white;
-  border: 0.3px solid rgba(0, 0, 0, 0.3);
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
-  text-decoration: none;
+  border: 1px solid rgb(178, 178, 178);
+  box-shadow: 0 2px 1px rgb(216, 216, 216);
   color: black;
+}
+
+.detailButton:hover {
+  background-color: #f5f5f5;
 }
 
 .communityItemTitle {
   font-size: 45px;
+}
+
+.bottomBox {
+  margin-top: 30px;
 }
 
 .likeRow {
@@ -457,8 +411,7 @@ export default {
   color: rgb(200, 200, 200)
 }
 
-.communityItemDeatil,
-.likesCount {
+.communityItemDeatil {
   font-size: 15px;
   font-weight: bold;
   color: rgb(164, 164, 164);
@@ -471,6 +424,8 @@ export default {
 }
 
 .likesCount {
+  margin-left: 10px;
+  font-size: 15px;
   color: black;
 }
 
